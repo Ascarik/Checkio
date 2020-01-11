98afc7b5e434e2b2ce6c57e8bbfaf0467c2f9ce0
Аскар <ascar@list.ru>
2020-01-11 16:31:00 +0300

    10 задача. Flatten a List

---
+++ home/10.py
@@ -0,0 +1,16 @@
+def flat_list(array):
+    list_items = []
+    for i in array:
+        if type(i) == type([]):
+            list_items.extend(flat_list(i))
+        else:
+            list_items.append(i)
+    return list_items
+
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
